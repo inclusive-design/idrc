0409ec3e70c0a6c326189efbde2c9f562ce3f8ef
Ned Zimmerman <ned@bight.dev>
2020-04-24 11:30:57 -0600

    fix: remove site data from helpers

--- src/admin/util.js
+++ src/admin/util.js
@@ -1,7 +1,6 @@
 import dateFilter from '../filters/date-filter';
 import markdownFilter from '../filters/markdown-filter';
 import w3DateFilter from '../filters/w3-date-filter';
-import site from '../_data/site.json';
 
 import slugify from 'slugify';
 
@@ -13,6 +12,5 @@ export {
 	dateFilter,
 	markdownFilter,
 	slugFilter,
-	w3DateFilter,
-	site
+	w3DateFilter
 };
