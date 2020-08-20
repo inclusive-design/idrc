67f2100b07edbd4646fb61ab75c903a999ef7fc9
Ned Zimmerman <ned@bight.dev>
2020-08-20 12:33:47 -0300

    fix: remove SCSS from Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,4 @@
 const mix = require('laravel-mix');
 
 mix.setPublicPath('./src/_includes/static')
-	.js('src/js/idrc.js', 'src/_includes/static/js')
-	.sass('src/scss/idrc.scss', 'src/_includes/static/css')
-	.options({
-		processCssUrls: false
-	})
-	.copyDirectory('src/fonts', 'src/_includes/static/fonts')
-	.copyDirectory('src/images', 'src/_includes/static/images');
+	.js('src/js/idrc.js', 'src/_includes/static/js');
