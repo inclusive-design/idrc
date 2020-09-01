710425f79b287fb4c1eb5f563843f0e9d762e9e0
Ned Zimmerman <ned@bight.dev>
2020-09-01 12:47:56 -0300

    ci: add Lighthouse audits

---
+++ .lighthouserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	ci: {
+		collect: {
+			settings: {
+				// Source the form factor from an environment variable set in the CI run
+				emulatedFormFactor: process.env.EMULATE_DEVICE || "mobile",
+				// Do not apply any throttling
+				throttlingMethod: "provided",
+				// Skipping "uses-http2" due to errors with reports see: http2 https://github.com/GoogleChrome/lighthouse/issues/6539
+				// Skipping "is-crawlable" because Netlify's preview preview for PRs add `x-robots-tag: noindex`
+				skipAudits: ["uses-http2", "is-crawlable"]
+			}
+		},
+		assert: {
+			assertions: {
+				"categories:performance": ["error", {"minScore": 0.9}],
+				"categories:accessibility": ["error", {"minScore": 1}],
+				"categories:best-practices": ["error", {"minScore": 0.9}],
+                "categories:seo": ["error", {"minScore": 0.9}],
+                "categories:pwa": ["error", {"minScore": 0.9}]
+			}
+		}
+	}
+};
