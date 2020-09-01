9cc141d25b14cd25346e73833593601bb8c89dbf
Ned Zimmerman <ned@bight.dev>
2020-09-01 16:19:41 -0300

    ci: stricter lighthouse rules, commit configuration

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -14,11 +14,11 @@ module.exports = {
 		},
 		assert: {
 			assertions: {
-				"categories:performance": ["error", {"minScore": 0.9}],
+				"categories:performance": ["error", {"minScore": 1}],
 				"categories:accessibility": ["error", {"minScore": 1}],
-				"categories:best-practices": ["error", {"minScore": 0.9}],
-                "categories:seo": ["error", {"minScore": 0.9}],
-                "categories:pwa": ["error", {"minScore": 0.9}]
+				"categories:best-practices": ["error", {"minScore": 1}],
+                "categories:seo": ["error", {"minScore": 1}],
+                "categories:pwa": ["error", {"minScore": 1}]
 			}
 		}
 	}
