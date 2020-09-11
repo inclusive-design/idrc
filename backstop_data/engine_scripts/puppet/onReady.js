1128a90a84f99c01d97c20abd3d985eb21657360
Ned Zimmerman <ned@bight.dev>
2020-09-11 14:29:21 -0300

    ci: load images eagerly to prevent false Backstop failures

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,5 +2,10 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
+  // Eagerly load images which would normally be lazy-loaded.
+  page.evaluate(async () => {
+    document.querySelectorAll('[loading="lazy"]').forEach((elem) => {
+      elem.loading = 'eager';
+    });
+  });
 };
