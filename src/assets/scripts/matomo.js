0b457a751786ab8480aee82a9f6d4804dc65b198
Ned Zimmerman <ned@bight.dev>
2021-01-06 11:31:41 -0400

    chore: update build process to align with starter kit

---
+++ src/assets/scripts/matomo.js
@@ -0,0 +1,25 @@
+/*
+Copyright the Inclusive Design Research Centre copyright holders.
+
+See the AUTHORS.md file at the top-level directory of this distribution and at
+https://github.com/inclusive-design/idrc/raw/main/AUTHORS.md.
+
+Licensed under the New BSD license. You may not use this file except in compliance with this License.
+
+You may obtain a copy of the New BSD License at
+https://github.com/inclusive-design/idrc/raw/main/LICENSE.md.
+*/
+
+"use strict";
+
+var _paq = window._paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function () {
+    var siteId = "3"; // Set this to the ID of your site in Matomo. Value must be quoted.
+    var u = "https://analytics.inclusivedesign.ca/";
+    _paq.push(["setTrackerUrl", u + "matomo.php"]);
+    _paq.push(["setSiteId", siteId]);
+    var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
+    g.type = "text/javascript"; g.async = true; g.defer = true; g.src = u + "matomo.js"; s.parentNode.insertBefore(g,s);
+})();
