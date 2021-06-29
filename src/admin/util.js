80877e4bd024dab5cacb4e4a696b87c2a07ade11
Ned Zimmerman <ned@bight.dev>
2021-06-29 11:08:36 -0300

    fix: restore CMS previews (#148)
    * fix: update preview configuration for Netlify CMS
    
    * fix: update Sass syntax

--- src/admin/util.js
+++ src/admin/util.js
@@ -1,8 +1,8 @@
-import formatDateFilter from '@fluid-project/eleventy-plugin-fluid/src/filters/format-date-filter';
-import isoDateFilter from '@fluid-project/eleventy-plugin-fluid/src/filters/iso-date-filter';
-import limitFilter from '@fluid-project/eleventy-plugin-fluid/src/filters/limit-filter';
-import markdownFilter from '@fluid-project/eleventy-plugin-fluid/src/filters/markdown-filter';
-import slugFilter from '@fluid-project/eleventy-plugin-fluid/src/filters/slug-filter';
+import formatDateFilter from 'eleventy-plugin-fluid/src/filters/format-date-filter';
+import isoDateFilter from 'eleventy-plugin-fluid/src/filters/iso-date-filter';
+import limitFilter from 'eleventy-plugin-fluid/src/filters/limit-filter';
+import markdownFilter from 'eleventy-plugin-fluid/src/filters/markdown-filter';
+import slugFilter from 'eleventy-plugin-fluid/src/filters/slug-filter';
 import splitFilter from '../filters/split-filter';
 import site from '../_data/site.json';
 
