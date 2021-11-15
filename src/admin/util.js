0a5f06476e29ea94fdf09d6f4b7d731b65dc6e0d
Justin Obara <obara.justin@gmail.com>
2021-11-15 13:37:01 -0500

    style: lint

--- src/admin/util.js
+++ src/admin/util.js
@@ -1,9 +1,9 @@
-import formatDateFilter from 'eleventy-plugin-fluid/src/filters/format-date-filter.js';
-import isoDateFilter from 'eleventy-plugin-fluid/src/filters/iso-date-filter.js';
-import limitFilter from 'eleventy-plugin-fluid/src/filters/limit-filter.js';
-import markdownFilter from 'eleventy-plugin-fluid/src/filters/markdown-filter.js';
-import slugFilter from 'eleventy-plugin-fluid/src/filters/slug-filter.js';
-import splitFilter from '../filters/split-filter.js';
+import formatDateFilter from 'eleventy-plugin-fluid/src/filters/format-date-filter';
+import isoDateFilter from 'eleventy-plugin-fluid/src/filters/iso-date-filter';
+import limitFilter from 'eleventy-plugin-fluid/src/filters/limit-filter';
+import markdownFilter from 'eleventy-plugin-fluid/src/filters/markdown-filter';
+import slugFilter from 'eleventy-plugin-fluid/src/filters/slug-filter';
+import splitFilter from '../filters/split-filter';
 import site from '../_data/site.json';
 
 export {
