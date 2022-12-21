7a9c1790f09b26a870ee2011cd613c3b5018c2e8
Cindy Qi Li <cli@ocadu.ca>
2022-12-21 15:15:27 -0500

    test: demonstrate an issue with Netlify CMS custom editor component

--- src/admin/previews.js
+++ src/admin/previews.js
@@ -235,3 +235,47 @@ CMS.registerPreviewTemplate('people', Person);
 CMS.registerPreviewTemplate('projects', Project);
 CMS.registerPreviewTemplate('tools', Tool);
 CMS.registerPreviewTemplate('site_data', SiteData);
+
+CMS.registerEditorComponent({
+	label: 'Image Positioned',
+	id: 'image-positioned',
+	fromBlock: match => {
+		console.log('fromBlock, match: ', match);
+		return match && {
+			image: match[2],
+			alt: match[1],
+			title: match[4]
+		};
+	},
+	toBlock: ({ alt, image, title }) => {
+		console.log('toBlock, image: ', image, '; alt: ', alt, '; title: ', title);
+	  	return `{% [${alt || ''}](${image || ''}${title ? ` "${title.replace(/"/g, '\\"')}"` : ''}) %}`;
+	},
+	// eslint-disable-next-line react/display-name
+	toPreview: ({ alt, image, title }, getAsset, fields) => {
+		console.log('toPreview, image: ', image, '; alt: ', alt, '; title: ', title);
+		const imageField = fields?.find(f => f.get('widget') === 'image');
+		const src = getAsset(image, imageField);
+		return <img src={src || ''} alt={alt || ''} title={title || ''} />;
+	},
+	pattern: /^{%\s\[(.*)\]\((.*?)(\s"(.*)")?\)\s%}$/,
+	fields: [
+	  {
+		label: 'Image',
+		name: 'image',
+		widget: 'image',
+		media_library: {
+		  allow_multiple: false,
+		},
+	  },
+	  {
+		label: 'Alt Text',
+		name: 'alt',
+	  },
+	  {
+		label: 'Title',
+		name: 'title',
+	  },
+	],
+});
+  
