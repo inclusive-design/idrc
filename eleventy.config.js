34dc96d63074274d1f439fdb3bcaa416eae7674d
Ned Zimmerman <ned@bight.dev>
2025-02-26 10:55:06 -0400

    fix: image bug

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,5 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import eleventyImage, { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import MarkdownIt from "markdown-it";
 
@@ -9,63 +9,91 @@ import parseTransform from "./src/_transforms/parse.js";
 import siteConfig from "./src/_data/config.json" with { type: "json" };
 import siteData from "./src/_data/site.json" with { type: "json" };
 
-export default eleventyConfig => {
-
+export default (eleventyConfig) => {
     const now = new Date();
 
     // Collections.
-    const livePosts = post => post.date <= now && !post.data.draft && !post.data.archived;
+    const livePosts = (post) =>
+        post.date <= now && !post.data.draft && !post.data.archived;
 
-    eleventyConfig.addCollection("people", collection => {
-        return collection.getFilteredByGlob("src/collections/people/*.md").sort((a, b) => {
-            const nameA = a.data.title;
-            const nameB = b.data.title;
+    eleventyConfig.addCollection("people", (collection) => {
+        return collection
+            .getFilteredByGlob("src/collections/people/*.md")
+            .sort((a, b) => {
+                const nameA = a.data.title;
+                const nameB = b.data.title;
 
-            if (nameA < nameB) {
-                return -1;
-            }
+                if (nameA < nameB) {
+                    return -1;
+                }
 
-            if (nameA > nameB) {
-                return 1;
-            }
+                if (nameA > nameB) {
+                    return 1;
+                }
 
-            return 0;
-        });
+                return 0;
+            });
     });
 
     for (const lang of siteConfig.locales) {
-        eleventyConfig.addCollection(`projects_${lang}`, collection => {
-            const projects = [...collection.getFilteredByGlob(`src/collections/projects/${lang}/*.md`)];
+        eleventyConfig.addCollection(`projects_${lang}`, (collection) => {
+            const projects = [
+                ...collection.getFilteredByGlob(
+                    `src/collections/projects/${lang}/*.md`
+                )
+            ];
 
-            return projects.sort((a, b) => Number.parseInt(b.data.order) - Number.parseInt(a.data.order)).reverse();
+            return projects
+                .sort(
+                    (a, b) =>
+                        Number.parseInt(b.data.order) - Number.parseInt(a.data.order)
+                )
+                .reverse();
         });
 
-        eleventyConfig.addCollection(`projectSubpages_${lang}`, collection => {
-            const projectSubpages = [...collection.getFilteredByGlob(`src/collections/project-subpages/${lang}/*.md`)];
+        eleventyConfig.addCollection(`projectSubpages_${lang}`, (collection) => {
+            const projectSubpages = [
+                ...collection.getFilteredByGlob(
+                    `src/collections/project-subpages/${lang}/*.md`
+                )
+            ];
 
-            return projectSubpages.sort((a, b) => Number.parseInt(b.data.order) - Number.parseInt(a.data.order)).reverse();
+            return projectSubpages
+                .sort(
+                    (a, b) =>
+                        Number.parseInt(b.data.order) - Number.parseInt(a.data.order)
+                )
+                .reverse();
         });
-    };
+    }
 
-    eleventyConfig.addCollection("news", collection => {
+    eleventyConfig.addCollection("news", (collection) => {
         return [
-            ...collection.getFilteredByGlob("./src/collections/news/*.md").filter(post => livePosts(post))
+            ...collection
+                .getFilteredByGlob("./src/collections/news/*.md")
+                .filter((post) => livePosts(post))
         ].reverse();
     });
 
-    eleventyConfig.addCollection("ideas", collection => {
+    eleventyConfig.addCollection("ideas", (collection) => {
         return [
-            ...collection.getFilteredByGlob("./src/collections/ideas/*.md").filter(post => livePosts(post))
+            ...collection
+                .getFilteredByGlob("./src/collections/ideas/*.md")
+                .filter((post) => livePosts(post))
         ].reverse();
     });
 
-    eleventyConfig.addCollection("postFeed", collection => {
-        return [...collection.getFilteredByGlob(["./src/collections/news/*.md", "./src/ideas/*.md"]).filter(post => livePosts(post))]
+    eleventyConfig.addCollection("postFeed", (collection) => {
+        return [
+            ...collection
+                .getFilteredByGlob(["./src/collections/news/*.md", "./src/ideas/*.md"])
+                .filter((post) => livePosts(post))
+        ]
             .reverse()
             .slice(0, 10);
     });
 
-    eleventyConfig.addCollection("resources", collection => {
+    eleventyConfig.addCollection("resources", (collection) => {
         return collection.getFilteredByGlob("src/collections/resources/*.md");
     });
 
@@ -80,9 +108,31 @@ export default eleventyConfig => {
     });
 
     eleventyConfig.addFilter("findByKey", (navItems, value) => {
-        return navItems.filter(item => {
+        return navItems.filter((item) => {
             return item.key === value;
         });
+    });
+
+    eleventyConfig.addShortcode("ideaImage", async (src, alt) => {
+        const html = await eleventyImage(`src/${src}`, {
+            formats: ["avif", "webp", "jpeg"],
+            returnType: "html",
+            outputDir: "_site/img/",
+            htmlOptions: {
+                imgAttributes: {
+                    alt,
+                    class: "thumbnail",
+                    "eleventy:ignore": "",
+                    loading: "lazy",
+                    decoding: "async"
+                }
+            },
+            transform: (sharp) => {
+                sharp.resize(570, 382, { fit: "cover" });
+            }
+        });
+
+        return html;
     });
 
     // Plugins.
@@ -93,14 +143,11 @@ export default eleventyConfig => {
                 loading: "lazy",
                 decoding: "async"
             }
-        },
-        transform: (sharp) => {
-            sharp.resize(570, 382, {fit: "cover"});
         }
     });
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(feedPlugin, {
-        type:"atom",
+        type: "atom",
         outputPath: "/feed.xml",
         collection: {
             name: "postFeed",
@@ -109,7 +156,7 @@ export default eleventyConfig => {
         metadata: {
             language: "en-CA",
             title: siteData.title,
-            base: `${ siteData.url }/`,
+            base: `${siteData.url}/`,
             author: {
                 name: siteData.name,
                 email: siteData.email
@@ -136,9 +183,9 @@ export default eleventyConfig => {
     eleventyConfig.addTransform("parse", parseTransform);
 
     // Passthrough file copy.
-    eleventyConfig.addPassthroughCopy({"src/assets/fonts": "assets/fonts"});
-    eleventyConfig.addPassthroughCopy({"src/assets/images": "assets/images"});
-    eleventyConfig.addPassthroughCopy({"src/media": "media"});
+    eleventyConfig.addPassthroughCopy({ "src/assets/fonts": "assets/fonts" });
+    eleventyConfig.addPassthroughCopy({ "src/assets/images": "assets/images" });
+    eleventyConfig.addPassthroughCopy({ "src/media": "media" });
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
 
     return {
