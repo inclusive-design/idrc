481563fb866143b5f2b8c5ac01995d0974c075b9
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-03-30 13:28:48 -0600

    fix: linting issue

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -2,5 +2,5 @@ import markdownlintConfig from '@inclusive-design/markdownlint-config';
 
 export default {
 	config: markdownlintConfig.config,
-	ignores: ["node_modules", "src/collections/**/*.md", "src/*.md", "CHANGELOG.md"]
+	ignores: ["node_modules", "src/collections/**/*.md", "src/*.md", "CHANGELOG.md"],
 };
